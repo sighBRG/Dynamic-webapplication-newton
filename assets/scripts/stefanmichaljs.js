465ac6c75f9b2242fba481ef8fab6af34680ceec
Stefan Michal <55457027+sighBRG@users.noreply.github.com>
2021-01-28 15:20:50 +0100

    Update stefanmichaljs.js

--- assets/scripts/stefanmichaljs.js
+++ assets/scripts/stefanmichaljs.js
@@ -1,9 +1,9 @@
-function scrollDown() {
-    let pageHeight = window.innerHeight;
-    let btn;
-    for (let s = 0; s < btn.length; s++) {
-        btn[s].addEventListener('click', function () {
-            window.scrollBy(0, pageHeight);
-        });
-    }
-}
+// function scrollDown() {
+//     let pageHeight = window.innerHeight;
+//     let btn;
+//     for (let s = 0; s < btn.length; s++) {
+//         btn[s].addEventListener('click', function () {
+//             window.scrollBy(0, pageHeight);
+//         });
+//     }
+// }
