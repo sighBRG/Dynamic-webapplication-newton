837425ee8e54360ca960492752a24a1e3c9dde95
dannycalos <dannycalos@outlook.com>
2021-01-29 09:28:14 +0100

    menubar styling added for navigation

--- assets/scripts/dannykalosportfolio.js
+++ assets/scripts/dannykalosportfolio.js
@@ -1,8 +1,11 @@
-function displayElement(element){
+function displayElement(text, menuButton){
     let childrenOfText = document.querySelector(".text").children;
     for(let i = 0 ; i < 7 ; i++){
         document.getElementById(childrenOfText[i].id).style.display = 'none';
+        document.getElementById("menuButton"+(i+1)).style.background = 'none';
     }
-    let textSection = document.getElementById(element);
+    let textSection = document.getElementById(text);
     textSection.style.display = 'block';
+    let buttonSelected = document.getElementById(menuButton);
+    buttonSelected.style.background = 'dodgerblue';
 }
