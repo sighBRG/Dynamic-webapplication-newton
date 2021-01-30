9c40351a7b42bbcdee3ac2120af1e3a16db41a36
dannycalos <dannycalos@outlook.com>
2021-01-30 22:57:38 +0100

    js code added to detect if user is on mobile in order to open phone icon

--- assets/scripts/dannykalosportfolio.js
+++ assets/scripts/dannykalosportfolio.js
@@ -1,6 +1,6 @@
 function displayElement(text, menuButton){
     let childrenOfText = document.querySelector(".text").children;
-    
+
     for(let i = 1 ; i < childrenOfText.length ; i++){
         document.getElementById(childrenOfText[i - 1].id).style.display = 'none';
         document.getElementById("menuButton"+(i)).style.background = 'none';
@@ -9,4 +9,10 @@ function displayElement(text, menuButton){
     textSection.style.display = 'block';
     let buttonSelected = document.getElementById(menuButton);
     buttonSelected.style.background = 'dodgerblue';
+}
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById("mobileIconTag").href = "0046736679360";
+}else{
+    document.getElementById("mobileIconTag").href = "#";
 }
