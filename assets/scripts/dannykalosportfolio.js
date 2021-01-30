a963c7ce67fb4d782ebeba0d22d96098a815be7f
dannycalos <dannycalos@outlook.com>
2021-01-30 22:33:26 +0100

    uneccesary code removed

--- assets/scripts/dannykalosportfolio.js
+++ assets/scripts/dannykalosportfolio.js
@@ -1,24 +1,24 @@
-$(window).resize(function() {
-    let icon = document.getElementById("icon");
-    let mobileNumber = document.getElementById("mobileNumber");
-    if(window.innerWidth < 670){
-        let aTag = document.createElement('a');
-        aTag.innerHTML = icon.outerHTML;
-        aTag.href = '0736679360';
-        aTag.id = 'aTagAdded';
-        icon.parentNode.insertBefore(aTag, icon);
-        icon.remove();
-        aTag.remove();
-    }
-    if(window.innerWidth > 669){
-        let tagAdded = document.getElementById("aTagAdded");
-        console.log(tagAdded);
-        if(tagAdded){
-        tagAdded.remove();
-        console.log(tagAdded);
-        }
-    }
-});
+// $(window).resize(function() {
+//     let icon = document.getElementById("icon");
+//     let mobileNumber = document.getElementById("mobileNumber");
+//     if(window.innerWidth < 670){
+//         let aTag = document.createElement('a');
+//         aTag.innerHTML = icon.outerHTML;
+//         aTag.href = '0736679360';
+//         aTag.id = 'aTagAdded';
+//         icon.parentNode.insertBefore(aTag, icon);
+//         icon.remove();
+//         aTag.remove();
+//     }
+//     if(window.innerWidth > 669){
+//         let tagAdded = document.getElementById("aTagAdded");
+//         console.log(tagAdded);
+//         if(tagAdded){
+//         tagAdded.remove();
+//         console.log(tagAdded);
+//         }
+//     }
+// });
 function displayElement(text, menuButton){
     let childrenOfText = document.querySelector(".text").children;
     for(let i = 0 ; i < 7 ; i++){
