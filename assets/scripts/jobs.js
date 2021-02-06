29caffbc2aa403ee296912fe81360e6da5cf95e9
dannycalos <dannycalos@outlook.com>
2021-02-06 19:51:38 +0100

    paging search results

--- assets/scripts/jobs.js
+++ assets/scripts/jobs.js
@@ -36,9 +36,13 @@ function replaceCharaterOfString(string, index, replacement) {
 let announcements = document.getElementsByClassName("announcements")[0];
 var sections = announcements.getElementsByTagName("section");
 var announcementsPerPage = 5;
+var searchResultIdArray = [];
+var page = parseInt(document.getElementById("page").innerHTML);
+var pageElement = document.getElementById("page");
+
 
 // adding some dummy data
-for(let i = 0 ; i < 15 ; i++){
+for(let i = 0 ; i < 18 ; i++){
     let section = fillAnnoucementInfo((i+1) + '. Javautvecklare söks', 'Newton', '2021-02-22', 'Newton@skola.se', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et porta nisl. Aenean maximus erat eu finibus tempor. Duis eu porttitor ligula. Aliquam mattis urna vitae enim ultricies, sagittis varius justo ornare. Curabitur lorem lorem, ullamcorper sit amet laoreet non, faucibus in lorem. Phasellus a ex suscipit, semper est quis, vestibulum metus. Curabitur sit amet accumsan eros. Aenean faucibus dui vel tortor consectetur hendrerit. Curabitur ornare condimentum nulla eu tempus. Aliquam erat volutpat. Nulla sed magna convallis, rutrum nisl sit amet, mattis erat. Suspendisse mollis pulvinar sapien, eu iaculis lacus congue in. Aenean ultrices urna eu felis feugiat porta. Maecenas libero purus, iaculis at luctus ac, varius in felis.');
     section.setAttribute("id", i);
     section.style.display = 'none';
@@ -53,12 +57,14 @@ function displayFirstPage(){
     for(let i = 0 ; i < announcementsPerPage ; i++){
         sections[i].style.display = 'block';
     }
+    page = 1
+    document.getElementById("page").innerHTML = page;
+    pageReference(page);
 }
 
 displayFirstPage();
 
 // search feature
-var searchResultIdArray = [];
 function searchJob(){
     let searchKey = document.getElementById("searchKey").value.toUpperCase();
     let title ;
@@ -67,6 +73,8 @@ function searchJob(){
         displayNone();
         displayFirstPage();
     }else{
+        page = 1;
+        document.getElementById("page").innerHTML = page;
         for (i = 0; i < sections.length; i++) {
             title = sections[i].firstChild.innerHTML.toUpperCase();
 
@@ -90,7 +98,7 @@ function displaySearchResult(){
             searchResultIdArray[i].style.display = 'block';
          }
     }
-    
+    pageReferenceSearchResult(page);  
 }
 
 // set all sections to display none
@@ -98,25 +106,54 @@ function displayNone(){
     for(let i = 0 ; i < sections.length ; i++){
         sections[i].style.display = 'none';
     }
+    
 }
 
 // paginantion
-var page = parseInt(document.getElementById("page").innerHTML);
-
 function pageReference(page){
     let leftArrow = document.getElementById("btn_prev");
     let rightArrow = document.getElementById("btn_next");
+    console.log("page "+ page);
     if(page == 1) {
         leftArrow.style.visibility = 'hidden';
         rightArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
     }else if(page == Math.ceil(sections.length / announcementsPerPage) ){
         rightArrow.style.visibility = 'hidden';
         leftArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
     }
     else{
         leftArrow.style.visibility = 'visible';
         rightArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
     }
+    
+}
+
+function pageReferenceSearchResult(page){
+    let leftArrow = document.getElementById("btn_prev");
+    let rightArrow = document.getElementById("btn_next");
+    console.log("searchPage "+page);
+    if(searchResultIdArray.length <= announcementsPerPage){
+        leftArrow.style.visibility = 'hidden';
+        rightArrow.style.visibility = 'hidden';
+        pageElement.style.visibility = 'hidden';
+    }else if(searchResultIdArray.length > announcementsPerPage && page == 1){
+        leftArrow.style.visibility = 'hidden';
+        rightArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
+    }else if(page == Math.ceil(searchResultIdArray.length / announcementsPerPage) ){
+        rightArrow.style.visibility = 'hidden';
+        leftArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
+    }
+    else{
+        leftArrow.style.visibility = 'visible';
+        rightArrow.style.visibility = 'visible';
+        pageElement.style.visibility = 'visible';
+    }
+
 }
 
 function getFirstIndexOfBlock(sections){
@@ -132,7 +169,8 @@ function getFirstIndexOfBlock(sections){
 function browseNext(){
     let firstOccurence = getFirstIndexOfBlock(sections);
     let firstOccurenceSearchResult = getFirstIndexOfBlock(searchResultIdArray);
-
+    page++;
+    document.getElementById("page").innerHTML = page;
     if(searchResultIdArray.length != 0){
         for(let i = 0; i < announcementsPerPage; i++){
             searchResultIdArray[i + firstOccurenceSearchResult].style.display = 'none';
@@ -140,6 +178,7 @@ function browseNext(){
                 searchResultIdArray[i + firstOccurenceSearchResult + announcementsPerPage].style.display = 'block';
             }
         }
+        pageReferenceSearchResult(page);
     }else{
         for(let i = 0; i < announcementsPerPage; i++){
             sections[i + firstOccurence].style.display = 'none';
@@ -147,11 +186,9 @@ function browseNext(){
                 sections[i + firstOccurence + announcementsPerPage].style.display = 'block';
             }
         }
+        pageReference(page);
     }
 
-    page++;
-    document.getElementById("page").innerHTML = page;
-    pageReference(page);
 } 
 
 function browsePrevious(){
