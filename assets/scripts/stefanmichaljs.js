d0048a891e56f9bff070a693708ecc25c86868a2
Stefan Michal <55457027+sighBRG@users.noreply.github.com>
2021-02-15 16:51:25 +0100

    Added animations to adds and sorted order of ads.

--- assets/scripts/stefanmichaljs.js
+++ assets/scripts/stefanmichaljs.js
@@ -7,3 +7,27 @@
 //         });
 //     }
 // }
+
+
+let ads = document.getElementsByClassName("inner-grid-sidebar")[0];
+let divs = ads.getElementsByTagName("div");
+
+function randomizeAds() {
+
+    let div = divs[0];
+    let img = ads.getElementsByTagName("img");
+
+    divs[0].remove();
+    ads.append(div);
+
+    for (let i = 0; i < img.length; i++) {
+        if (i === 0) {
+            $(divs[0]).delay(3000).fadeOut(2000);
+        } else {
+            $(divs[0]).fadeIn(750);
+        }
+    }
+}
+
+setInterval(randomizeAds, 5000);
+
