b16e74948861ef769ab82e183cdacb4f4472e62b
TadevosEliasyan <57492281+TadevosEliasyan@users.noreply.github.com>
2021-02-16 15:45:30 +0100

    updatera cv

---
+++ assets/scripts/tadevos.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    alert("Om vill ha mer information om Högskolan go till: newton.se");
+  }
+  function myFunction1() {
+    alert("Om vill ha mer information om Universitet go till: anau.am");
+  }
+  function myFunction2() {
+    alert("Om vill ha mer information om Skolan go till: hermods.se");
+  }
+  function myFunction3() {
+    alert("Om vill ha mer information om Företaget go till: adlbris.se");
+  }
+  function myFunction4() {
+    alert("Om vill ha mer information om Företaget go till: heby.se");
+  }
+  function myFunction5() {
+    alert("Om vill ha mer information om Företaget go till: effect.se");
+  }
+  function myFunction6() {
+    alert("Om vill ha mer information om Företaget go till: visma.se");
+  }
+  
